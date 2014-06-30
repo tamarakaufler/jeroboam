2d098537f4786d85963869eaf1eb6cd990dc6426
Tamara Kaufler <tkaufler@gmail.com>
2014-06-30 22:22:02 +0100

    initial commit of FakeMagnolia

---
+++ FakeMagnolia/insert_tokens.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/perl
+use v5.14;
+use autodie;
+
+use MongoDB;
+use MongoDB::OID;
+
+use Data::Dumper qw(Dumper);
+
+## connect to mongodb,database uc_token_translations db
+##	properties: token, brand, page, locale, value
+
+my $mongo_conn   = MongoDB::Connection->new;
+my $mongo_db 	 = $mongo_conn->uc_token_translations;
+my $tokens_table = $mongo_db->tokens;
+
+my $tokens_on_demand = $tokens_table->find({ brand => 'default',
+                                       page  => 'billing',
+                                       locale => 'en' });
+
+while (my $page_tokens = $tokens_on_demand->next) {
+	say Dumper($page_tokens);
+}
+
+#exit;
+
+##==============================================================
+
+my $input = {
+           "global" => 
+                { 
+                  "CURRENCY.SYMBOL" => "£££",
+                  "CURRENCY.POSITION" => "BEFORE",
+                  "CURRENCY.SYMBOL_ISO_4217" => "GBP",
+                  "TOKENS_LIVE_CHAT" => "I am so chatty",
+                  "URLS_TERMS" => "I shall deal with you on my terms",
+                  "META_PHONE_NUMBER" => "Call us!!!",
+                  "META_EULARIAN" => "photobox-uk",
+		  "ERRORS.NO_BASKET" => "There is no basket!!",
+		  "ERRORS.NOT_LOGGED_IN" => "Log in, you rascal :O",
+		  "ERRORS.MISMATCH_PASSWORD" => "They are different!!",
+		  "ERRORS.EMPTY_BASKET" => "Why is your basket empty!!",
+                  "TOKENS_PROGRESS_BAR_STAGE_1" => "Clubs",
+                  "TOKENS_PROGRESS_BAR_STAGE_2" => "Spades",
+                  "TOKENS_PROGRESS_BAR_STAGE_3" => "Hearts",
+                  "TOKENS_PROGRESS_BAR_STAGE_4" => "Diamonds",
+                  "TOKENS_PROGRESS_BAR_STAGE_5" => "Stars"
+                },  
+             "billing" =>
+                {   
+                  "TOKENS_TITLE" => "I am a billing page",
+                  "META_ORDER_NEXT_STEP" => "Head for hell ",
+                  "FIELDS_POSTCODE" => "Hello - your postcode",
+                  "META_FIND_ADDRESS" => "Let's search",
+                  "META_MANUAL_INPUT_ADDRESS" => "Input your address or else ",
+                  "META_AUTO_INPUT_ADDRESS" => "We can do it for you "
+                },  
+             "login" =>
+                { "TOKENS_TITLE" => "I am a login page",
+                  "META_SHIPPING" => "I am Shipping",
+                  "META_ITEM_SINGLE" => "I am single item",
+                  "FIELD_EMAIL" => "Your email please",
+                  "FIELD_PASSWORD" => "Your password please"
+                },  
+             "deliverymethods" =>
+                {   
+                  "META_SHIPPING" => "I am Shipping",
+                  "META_ITEM_SINGLE" => "I am that"
+                },
+             "delivery" =>
+                {
+                  "META_DELIV_NEXT_STEP" => "Head for hell ",
+                  "TOKENS_DELIVERY_TITLE" => "I am Funny Delivery",
+                  "TOKENS_SELECT_DELIVERY" => "Really funny delivery",
+                  "TOKENS_TITLE" => "I am a delivery page",
+                  "TOKENS_BILLING_TITLE" => "I shall bill you",
+                  "META_SHIPPING" => "I am Shipping3",
+                  "META_ITEM_SINGLE" => "I am single item"
+                },
+             "review" =>
+                {
+                  "META_SUBMIT_PAYMENT" => "FIRE!!!!"
+                },
+             "address_lookup" =>
+                {
+                  "META_SELECT_YOUR_ADDRESS" => "SELECT your ADDRESS"
+                },
+             "addresses_add" =>
+                {
+                  "FIELDS_FIRST_NAME" => "Give me your first name, buster",
+                  "FIELDS_FIRST_NAME" => "Give me your last name, buster",
+                  "META_SAVE_ADDRESS" => "Now save it",
+                  "META_FIND_ADDRESS" => "Where is my address? Fetch it!"
+                }
+            };
+
+foreach my $page (keys %$input) {
+	foreach my $token (keys %{$input->{$page}}) {
+	    my $data = {
+				"brand"  => 'default',
+				"page"   => $page,
+				"locale" => 'en',
+				"token"  => $token,
+				"value"  => $input->{$page}{$token},
+	    };
+
+	    print Dumper($data);
+
+	    $tokens_table->insert($data);
+	}
+}
+
---
+++ FakeMagnolia/magnolia_mock.pl
@@ -0,0 +1,135 @@
+use Mojolicious::Lite;
+
+use MongoDB;
+use MongoDB::OID;
+use URI::Escape;
+use Data::Dumper qw(Dumper);
+
+use encoding 'utf8';
+
+## connect to mongodb,database uc_token_translations db
+##	properties: token, brand, page, locale, value
+
+my $mongo_conn = MongoDB::Connection->new;
+my $mongo_db   = $mongo_conn->uc_token_translations;
+my $tokens_table     = $mongo_db->tokens;
+
+get '/magnolia_mock/:brand/:page/:locale' => sub {
+  my $self = shift;
+
+  my ($brand, $page, $locale) = ($self->param('brand'), $self->param('page'), $self->param('locale'));
+  my @tokens = _get_tokens($brand, $locale, $page);
+
+  $self->stash(tokens => $tokens[0]);
+  $self->render('magnolia_mockbrandpagelocale', { brand  => $brand, page => $page, locale => $locale, });
+};
+
+get '/magnolia_mock2/save/:id/:value' => sub {
+  my $self = shift;
+
+say "in magnolia_mock2/save";
+
+  my $token_id    = $self->param('id');
+  my $token_value = uri_unescape($self->param('value'));
+
+  my $token = $tokens_table->find_one({_id => MongoDB::OID->new(value => $token_id)});
+
+  say "\t old value = $token->{value}";
+
+  $tokens_table->update( { _id => MongoDB::OID->new(value => $token_id) }, { '$set' => { value => $token_value } } );
+  my $token_after = $tokens_table->find_one({ _id => MongoDB::OID->new(value => $token_id) });
+
+  say "\t new value = $token_after->{value}";
+
+  $self->render(json => { id => $token_id, value => $token_after->{value}});
+
+};
+
+get '/magnolia_api_mock/:brand/:locale' => sub {
+  my $self = shift;
+
+  my ($brand, $locale) = ($self->param('brand'), $self->param('locale'));
+  my @tokens = _get_tokens($brand, $locale);
+
+  $self->render(json => $tokens[1]);
+};
+
+sub _get_tokens {
+	my ($brand, $locale, $page) = @_;
+
+	my $search = {};
+	if ($brand) {
+		$search->{brand} = $brand;
+	}
+	elsif ($locale) {
+		$search->{locale} = $locale;
+	}
+	elsif ($page) {
+		$search->{page} = $page;
+	}
+
+	print Dumper($search);
+
+        my $tokens_on_demand = $tokens_table->find($search);
+
+	my $found4page 	= [];
+	my $json  	= {};
+
+	while (my $page_token = $tokens_on_demand->next) {
+		my $name = $page_token->{token}; 
+		$name    =~ s/_/./; 
+		my $token = { 
+			   name   => $name,
+			   value  => $page_token->{value},
+			   id	  => $page_token->{_id}{value},
+			 };
+		push @$found4page, $token if $page && $page eq $page_token->{page};
+
+		$json->{ $page_token->{page} }{ $name }{ value } = $page_token->{value};
+	}
+	
+	return ($found4page, $json);
+}
+
+app->start;
+__DATA__
+
+@@ magnolia_mockbrandpagelocale.html.ep
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Mock Magnolia - Mojolicious rocks!</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+  <script>
+	$(document).ready(function() {
+		  $(".update_token").click(function(){
+			var token_id   = $(this).attr('token_id');
+			var token_value=$("input[id='" + token_id + "']").val();
+			// alert(token_id  + ' - ' + token_value);
+	
+			var jqxhr = $.get( "http://127.0.0.1:3000/magnolia_mock2/save/" + token_id + '/' + encodeURI(token_value), function( data ) {
+				alert(data);
+		  	}).fail(function(jqxhr, error) { error });
+
+		  }); 
+	  }); 
+  </script>
+  </head>
+  <body>
+
+  <h2>Brand <%= $brand %> - Page <%= $page %> - Locale <%= $locale %></h2>
+
+	<table>
+		% for my $token (@$tokens) {
+		<tr>
+  			<td><%= $token->{name} %></td>
+			<td><input id="<%=$token->{id} %>" type="text" value="<%= $token->{value} %>"></td>
+			<td><button class="update_token" token_id="<%= $token->{id} %>">Update</button></td>
+		</tr>
+		% }
+	</table>
+
+  </body>
+</html>
